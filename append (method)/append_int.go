f96b76b3cf2126d7de0760dd8c7275d0932540e3
beardm4n <dmitriykorovko@gmail.com>
2023-01-19 03:31:35 +0300

    create append method

---
+++ append (method)/append_int.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("hyi")
+	list := make([]int, 4, 6)
+
+	list = Append(list, 9)
+	list = Append(list, 8)
+	list = Append(list, 7)
+	
+	fmt.Println("list", list, len(list), cap(list))
+}
+
+func Append(list []int, el int) []int {
+	var res []int
+
+	resLen := len(list) + 1
+
+	if (resLen <= cap(list)) {
+		res = list[:resLen]
+	} else {
+		resCap := resLen
+
+		if resCap < 2 * len(list) {
+			resCap = 2 * len(list)
+		}
+
+		res = make([]int, resLen, resCap)
+
+		copy(res, list)
+	}
+
+	res[len(list)] = el
+
+	return res
+}
